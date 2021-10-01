73c821cfbf66d66dab0adc8b7b0ce8cc3ab47061
Brandon Plank <52553007+BrandonPlank@users.noreply.github.com>
2021-10-01 19:20:11 -0400

    Update routes.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -133,7 +133,7 @@ func routes(_ app: Application) throws {
                 210 < 100 == false 10 > 110 == false, 200 window
             */
 
-             if (time + 100 < score || time - 100 > score) && score > 1000 {
+             if (time + 100 < score || time - 100 > score) /* && score > 1000 */ {
                  user.isBanned = true
                  user.banReason = "Cheating (Anticheat)"
                  let _ = user.update(on: req.db) .map { user }
