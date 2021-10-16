aa92ac402468d975dff17716ebec82c40de7e0fc
Brandon Plank <52553007+BrandonPlank@users.noreply.github.com>
2021-10-15 20:38:46 -0400

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,7 +7,7 @@ import Gatekeeper
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     //app.middleware.use(UserAuthenticator())
     
     app.caches.use(.memory)
